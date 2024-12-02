7d2f863b515103f34a14a1ee02cc57bc8d2f0144
yubanmeiqin9048 <1414603524@qq.com>
2024-12-02 23:40:14 +0800

    feat(autosubset): 支持v2
    - 在 package.json 中添加 v2 版本标识

--- plugins/autosubset/__init__.py
+++ plugins/autosubset/__init__.py
@@ -3,13 +3,14 @@ import re
 import shutil
 import subprocess
 from pathlib import Path
-from typing import Any, List, Dict, Tuple
+from typing import Any, Dict, List, Tuple
 
-from app.utils.system import SystemUtils
-from app.core.event import eventmanager, Event
-from app.schemas.types import EventType
-from app.plugins import _PluginBase
+from app.core.event import Event, eventmanager
 from app.log import logger
+from app.plugins import _PluginBase
+from app.schemas.types import EventType
+from app.utils.system import SystemUtils
+
 
 class AutoSubset(_PluginBase):
     # 插件名称
@@ -38,7 +39,7 @@ class AutoSubset(_PluginBase):
     _fontpath = None
     _binaryname = None
     _overwrite = False
-    _fontrename = False 
+    _fontrename = False
     _hdrluminance = False
     _sethdrluminance = False
     _deletesubfontfolder = False
@@ -52,8 +53,11 @@ class AutoSubset(_PluginBase):
             self._hdrluminance = config.get("hdrluminance")
             self._deletesubfontfolder = config.get("deletesubfontfolder")
             self._afpath = self.get_data_path()
-            self._binaryname = 'assfonts'
-            if not Path(self._fontpath).exists() or not Path(f'{self._afpath}/{self._binaryname}').exists():
+            self._binaryname = "assfonts"
+            if (
+                not Path(self._fontpath).exists()
+                or not Path(f"{self._afpath}/{self._binaryname}").exists()
+            ):
                 self._enabled = False
                 self.__update_config()
                 logger.error("未配置字体库或assfonts可执行版本不存在，插件退出")
@@ -63,7 +67,7 @@ class AutoSubset(_PluginBase):
 
     def get_state(self) -> bool:
         return self._enabled
-    
+
     @staticmethod
     def get_command() -> List[Dict[str, Any]]:
         pass
@@ -80,182 +84,167 @@ class AutoSubset(_PluginBase):
         """
         return [
             {
-                'component': 'VForm',
-                'content': [
+                "component": "VForm",
+                "content": [
                     {
-                        'component': 'VRow',
-                        'content': [
+                        "component": "VRow",
+                        "content": [
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 6
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 6},
+                                "content": [
                                     {
-                                        'component': 'VTextField',
-                                        'props': {
-                                            'model': 'fontpath',
-                                            'label': '字体库'
-                                        }
+                                        "component": "VTextField",
+                                        "props": {
+                                            "model": "fontpath",
+                                            "label": "字体库",
+                                        },
                                     }
-                                ]
+                                ],
                             },
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 6
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 6},
+                                "content": [
                                     {
-                                        'component': 'VTextField',
-                                        'props': {
-                                            'model': 'version',
-                                            'readonly': True,
-                                            'label': 'assfonts版本',
-                                            'placeholder': '暂未安装'
-                                        }
+                                        "component": "VTextField",
+                                        "props": {
+                                            "model": "version",
+                                            "readonly": True,
+                                            "label": "assfonts版本",
+                                            "placeholder": "暂未安装",
+                                        },
                                     }
-                                ]
-                            }
-                        ]
+                                ],
+                            },
+                        ],
                     },
                     {
-                        'component': 'VRow',
-                        'content': [
+                        "component": "VRow",
+                        "content": [
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
                                     {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'enabled',
-                                            'label': '启用',
-                                        }
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "enabled",
+                                            "label": "启用",
+                                        },
                                     }
-                                ]
+                                ],
                             },
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
                                     {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'fontrename',
-                                            'label': '字体重命名',
-                                        }
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "fontrename",
+                                            "label": "字体重命名",
+                                        },
                                     }
-                                ]
+                                ],
                             },
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
                                     {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'overwrite',
-                                            'label': '覆盖原文件',
-                                        }
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "overwrite",
+                                            "label": "覆盖原文件",
+                                        },
                                     }
-                                ]
+                                ],
                             },
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
                                     {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'hdrluminance',
-                                            'label': '调整字幕HDR亮度',
-                                        }
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "hdrluminance",
+                                            "label": "调整字幕HDR亮度",
+                                        },
                                     }
-                                ]
+                                ],
                             },
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
                                     {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'deletesubfontfolder',
-                                            'label': '删除子集化后的字体',
-                                        }
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "deletesubfontfolder",
+                                            "label": "删除子集化后的字体",
+                                        },
                                     }
-                                ]
-                            }
-                        ]
+                                ],
+                            },
+                        ],
                     },
                     {
-                        'component': 'VRow',
-                        'content': [
+                        "component": "VRow",
+                        "content": [
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
+                                "component": "VCol",
+                                "props": {
+                                    "cols": 12,
                                 },
-                                'content': [
+                                "content": [
                                     {
-                                        'component': 'VAlert',
-                                        'props': {
-                                            'type': 'info',
-                                            'variant': 'tonal',
-                                            'text': '请自行下载assfonts二进制文件到插件数据目录'
-                                        }
+                                        "component": "VAlert",
+                                        "props": {
+                                            "type": "info",
+                                            "variant": "tonal",
+                                            "text": "请自行下载assfonts二进制文件到插件数据目录",
+                                        },
                                     }
-                                ]
+                                ],
                             }
-                        ]
-                    }
-                ]
+                        ],
+                    },
+                ],
             }
         ], {
             "enabled": False,
             "version": None,
             "fontpath": None,
-            "overwrite":False,
+            "overwrite": False,
             "fontrename": False,
             "hdrluminance": False,
-            "deletesubfontfolder": False
+            "deletesubfontfolder": False,
         }
-    
+
     def __update_config(self):
-        self.update_config({
-            "enabled": self._enabled,
-            "version": self.__get_version(),
-            "fontpath": self._fontpath,
-            "overwrite": self._overwrite,
-            "fontrename": self._fontrename,
-            "hdrluminance": self._hdrluminance,
-            "deletesubfontfolder": self._deletesubfontfolder
-        })
+        self.update_config(
+            {
+                "enabled": self._enabled,
+                "version": self.__get_version(),
+                "fontpath": self._fontpath,
+                "overwrite": self._overwrite,
+                "fontrename": self._fontrename,
+                "hdrluminance": self._hdrluminance,
+                "deletesubfontfolder": self._deletesubfontfolder,
+            }
+        )
 
     def __init_assfonts(self):
-        os.popen(f'cd {self._afpath} && ./{self._binaryname} -b -f {self._fontpath} -d {self._fontpath}')
+        os.popen(
+            f"cd {self._afpath} && ./{self._binaryname} -b -f {self._fontpath} -d {self._fontpath}"
+        )
 
     def __get_version(self):
-        if Path(f'{self._afpath}/{self._binaryname}').exists():
-            self._version = SystemUtils.execute(f'cd {self._afpath} && chmod a+x {self._binaryname} && ./{self._binaryname}').split(" ")[1]
+        if Path(f"{self._afpath}/{self._binaryname}").exists():
+            self._version = SystemUtils.execute(
+                f"cd {self._afpath} && chmod a+x {self._binaryname} && ./{self._binaryname}"
+            ).split(" ")[1]
         return self._version
 
     def __overwrite_original_file(self, file: str, assfont_rename: bool):
@@ -291,8 +280,10 @@ class AutoSubset(_PluginBase):
     def __build_af_command(self, input_ass: Path) -> List[str]:
         af_command = [
             f"{self._afpath}/{self._binaryname}",
-            "-i", str(input_ass),
-            "-d", self._fontpath
+            "-i",
+            str(input_ass),
+            "-d",
+            self._fontpath,
         ]
         if self._fontrename:
             af_command.append("-r")
@@ -314,33 +305,47 @@ class AutoSubset(_PluginBase):
 
     @eventmanager.register(EventType.TransferComplete)
     def task_in(self, event: Event):
-        #获取要处理的文件路径
-        work_file = Path(event.event_data['transferinfo'].target_path)
-        #获取媒体质量
-        media_edition = event.event_data['meta'].edition
+        # 获取要处理的文件路径
+        work_file = Path(event.event_data["transferinfo"].target_path)
+        # 获取媒体质量
+        media_edition = event.event_data["meta"].edition
         if "hdr" in media_edition and self._hdrluminance:
             self._sethdrluminance = True
         # 获取同目录下的ass文件
-        ass_list = [ass for ass in work_file.parent.rglob("*.ass") if ".assfonts." not in ass.name]        
+        ass_list = [
+            ass
+            for ass in work_file.parent.rglob("*.ass")
+            if ".assfonts." not in ass.name
+        ]
         for input_ass in ass_list:
             # 执行命令:
             if self.__subset_ass_file(input_ass=input_ass):
                 logger.info(f"{input_ass.name} 子集化失败")
                 return
             logger.info(f"{input_ass.name} 子集化成功")
-            
+
             # 处理完字幕后，如果设置了删除字体文件夹选项，则删除该文件夹
             if self._deletesubfontfolder:
                 font_folder_name = input_ass.stem + "_subsetted"
-                self.__delete_subsetfont_folder(font_folder_path=os.path.join(str(input_ass.parent), font_folder_name))
-                
+                self.__delete_subsetfont_folder(
+                    font_folder_path=os.path.join(
+                        str(input_ass.parent), font_folder_name
+                    )
+                )
+
             # 处理完字幕后，删除纯重名文件
             if self._fontrename:
                 plain_rename_file = input_ass.stem + ".rename.ass"
-                self.__delete_plain_ass(plain_rename_file=os.path.join(str(input_ass.parent), plain_rename_file))
+                self.__delete_plain_ass(
+                    plain_rename_file=os.path.join(
+                        str(input_ass.parent), plain_rename_file
+                    )
+                )
 
             if self._overwrite:
-                self.__overwrite_original_file(file=str(input_ass), assfont_rename=self._fontrename)
+                self.__overwrite_original_file(
+                    file=str(input_ass), assfont_rename=self._fontrename
+                )
 
     def stop_service(self):
         """
