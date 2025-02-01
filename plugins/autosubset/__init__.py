c5c5c4a1fff289d18a64bfdef3473fa1a2a0a83e
yubanmeiqin9048 <1414603524@qq.com>
2025-02-01 21:29:57 +0800

    refactor(autosubset): 重构 AutoSubset 插件
    - 移除了冗余代码，提高了代码执行效率

--- plugins/autosubset/__init__.py
+++ plugins/autosubset/__init__.py
@@ -20,7 +20,7 @@ class AutoSubset(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/yubanmeiqin9048/MoviePilot-Plugins/main/icons/Assfonts.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "yubanmeiqin9048"
     # 作者主页
@@ -247,25 +247,39 @@ class AutoSubset(_PluginBase):
             ).split(" ")[1]
         return self._version
 
-    def __overwrite_original_file(self, file: str, assfont_rename: bool):
-        if os.path.exists(file):
-            os.remove(file)
-        if assfont_rename:
-            assfont_file = os.path.splitext(file)[0] + ".rename.assfonts.ass"
-        else:
-            assfont_file = os.path.splitext(file)[0] + ".assfonts.ass"
-        if os.path.exists(assfont_file):
-            os.rename(assfont_file, file)
+    def __process_ass(self, ass_file: Path):
+        try:
+            self.__init_assfonts()
+            cmd = self.__build_af_command()
+            result = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
 
-    def __delete_subsetfont_folder(self, font_folder_path: str):
-        if os.path.exists(font_folder_path):
-            shutil.rmtree(font_folder_path)
+            if self.__check_errors(result.stdout):
+                return False
 
-    def __delete_plain_ass(self, plain_rename_file: str):
-        if os.path.exists(plain_rename_file):
-            os.remove(plain_rename_file)
+            if self._deletesubfontfolder:
+                shutil.rmtree(
+                    ass_file.parent / f"{ass_file.stem}_subsetted", ignore_errors=True
+                )
 
-    def __assfonts_shell(self, text: str):
+            if self._fontrename:
+                (ass_file.parent / f"{ass_file.stem}.rename.ass").unlink(
+                    missing_ok=True
+                )
+
+            if self._overwrite:
+                new_file = ass_file.with_name(
+                    f"{ass_file.stem}{'.rename' if self._fontrename else ''}.assfonts.ass"
+                )
+                if new_file.exists():
+                    new_file.replace(ass_file)
+
+            return True
+
+        except Exception as e:
+            logger.error(f"处理 {ass_file.name} 失败: {e}")
+            return False
+
+    def __check_errors(self, text: str):
         if "[ERROR]" in text:
             error_lines = [line for line in text.splitlines() if "[ERROR]" in line]
             for line in error_lines:
@@ -274,8 +288,8 @@ class AutoSubset(_PluginBase):
                 missing_fonts = re.findall(r'Missing the font: "([^"]+)"', text)
                 if missing_fonts:
                     logger.error(f"字体缺失: {','.join(missing_fonts)}")
-            return 1
-        return 0
+            return False
+        return True
 
     def __build_af_command(self, input_ass: Path) -> List[str]:
         af_command = [
@@ -291,61 +305,21 @@ class AutoSubset(_PluginBase):
             af_command.append("-l")
         return af_command
 
-    def __subset_ass_file(self, input_ass: Path):
-        try:
-            self.__init_assfonts()
-            af_command = self.__build_af_command(input_ass)
-            result = subprocess.run(af_command, stdout=subprocess.PIPE, text=True)
-
-            return self.__assfonts_shell(text=result.stdout)
-
-        except Exception as e:
-            logger.error(f"处理文件 {input_ass} 时发生错误: {e}")
-            return 1
-
     @eventmanager.register(EventType.TransferComplete)
     def task_in(self, event: Event):
-        # 获取要处理的文件路径
-        work_file = Path(event.event_data["transferinfo"].target_path)
-        # 获取媒体质量
-        media_edition = event.event_data["meta"].edition
-        if "hdr" in media_edition and self._hdrluminance:
-            self._sethdrluminance = True
-        # 获取同目录下的ass文件
-        ass_list = [
-            ass
-            for ass in work_file.parent.rglob("*.ass")
-            if ".assfonts." not in ass.name
-        ]
-        for input_ass in ass_list:
-            # 执行命令:
-            if self.__subset_ass_file(input_ass=input_ass):
-                logger.info(f"{input_ass.name} 子集化失败")
-                return
-            logger.info(f"{input_ass.name} 子集化成功")
+        target = Path(event.event_data["transferinfo"].target_path)
+        self._sethdrluminance = (
+            "hdr" in event.event_data["meta"].edition and self._hdrluminance
+        )
 
-            # 处理完字幕后，如果设置了删除字体文件夹选项，则删除该文件夹
-            if self._deletesubfontfolder:
-                font_folder_name = input_ass.stem + "_subsetted"
-                self.__delete_subsetfont_folder(
-                    font_folder_path=os.path.join(
-                        str(input_ass.parent), font_folder_name
-                    )
-                )
+        for ass in target.parent.glob("**/*.ass"):
+            if ".assfonts." in ass.name:
+                continue
 
-            # 处理完字幕后，删除纯重名文件
-            if self._fontrename:
-                plain_rename_file = input_ass.stem + ".rename.ass"
-                self.__delete_plain_ass(
-                    plain_rename_file=os.path.join(
-                        str(input_ass.parent), plain_rename_file
-                    )
-                )
-
-            if self._overwrite:
-                self.__overwrite_original_file(
-                    file=str(input_ass), assfont_rename=self._fontrename
-                )
+            if self.__process_ass(ass):
+                logger.info(f"{ass.name} 处理成功")
+            else:
+                logger.warning(f"{ass.name} 处理失败")
 
     def stop_service(self):
         """
