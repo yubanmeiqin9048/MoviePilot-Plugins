27730ab035fae4512e33dc115d333eb64017f5a7
yubanmeiqin9048 <1414603524@qq.com>
2025-08-24 21:30:49 +0800

    chore(package): 更新下载api版本号

--- plugins.v2/downloaderapi/__init__.py
+++ plugins.v2/downloaderapi/__init__.py
@@ -22,7 +22,7 @@ class DownloaderApi(_PluginBase):
     # 插件图标
     plugin_icon = "sync_file.png"
     # 插件版本
-    plugin_version = "1.3"
+    plugin_version = "1.3.1"
     # 插件作者
     plugin_author = "yubanmeiqin9048"
     # 作者主页
